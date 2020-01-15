afbd2763df375bd897d01e40a7977e76c2ac9692
ryzhak <ryzhak.vladimir@gmail.com>
2020-01-15 16:23:18 +0300

    feat: add basic android lib

---
+++ example/PushNotification.js
@@ -0,0 +1,5 @@
+import { NativeModules } from 'react-native';
+
+module.exports = {
+	PushNotificationAndroid: NativeModules.PushNotificationAndroid
+};
--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -1,11 +1,9 @@
 package com.example;
 
 import android.app.Application;
-import android.util.Log;
 
+import com.cryptoticket.reactnativepushnotification.PushNotificationPackage;
 import com.facebook.react.PackageList;
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+      packages.add(new PushNotificationPackage());
       return packages;
     }
 
